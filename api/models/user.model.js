1b3c6f35ad6f0be223a4280a5be89f0ce7f886a9
Dhruvil Nakrani <128470872+nd5a@users.noreply.github.com>
2025-07-13 14:41:28 +0530

    Add files via upload

---
+++ api/models/user.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      default:
+        "https://icons.veryicon.com/png/o/miscellaneous/two-color-webpage-small-icon/user-244.png",
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
